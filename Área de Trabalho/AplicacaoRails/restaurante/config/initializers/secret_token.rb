09a1662eeea918fce555077474becd4b6cdfed4d
Valeria <valeria@valeria-laptop.(none)>
2013-04-02 18:10:11 -0300

    Alterando o GemFile

---
+++ Área de Trabalho/AplicacaoRails/restaurante/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restaurante::Application.config.secret_token = '5d1718c7cf04aa8e244db30e19924456e3cf3e499cba09a0994fc8708d7428994813bbb798c15fd511156d4b21d103f2373c513903cd78737523ba4377d14514'
